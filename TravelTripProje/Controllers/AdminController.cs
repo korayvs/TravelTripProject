a0742b5588bc074682c4ba0850dac588d0b16ee6
Koray Vatansever <koray-vatansever@hotmail.com>
2025-01-18 21:40:16 +0300

    admin updated

---
+++ TravelTripProje/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p)
+        {
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -11,8 +11,8 @@ namespace TravelTripProje.Controllers
     {
         Context c = new Context();
         public ActionResult Index()
-        {            
-            var degerler = c.Blogs.ToList();
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.ID).Take(5).ToList();
             return View(degerler);
         }
 
@@ -30,6 +30,24 @@ namespace TravelTripProje.Controllers
         public PartialViewResult Partial2()
         {
             var deger = c.Blogs.Where(x => x.ID == 3).ToList();
+            return PartialView(deger);
+        }
+
+        public PartialViewResult Partial3()
+        {
+            var deger = c.Blogs.ToList();
+            return PartialView(deger);
+        }
+
+        public PartialViewResult Partial4()
+        {
+            var deger = c.Blogs.Take(3).ToList();
+            return PartialView(deger);
+        }
+
+        public PartialViewResult Partial5()
+        {
+            var deger = c.Blogs.Take(3).OrderByDescending(x => x.ID).ToList();
             return PartialView(deger);
         }
     }
